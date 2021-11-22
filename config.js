9afd07756ff1e15172fc4e6c8525194891f73623
Andrew <90209255+AndzejusN@users.noreply.github.com>
2021-11-22 21:13:45 +0200

    Second commit

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// const constants = {
-//     openWeatherMap:{
-//         BASE_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
-//         SECRET_KEY: 'eef1a736509553c562e025e8c69d4e6b'
-//     }
-// }
-//
-// module.exports = constants;
+const constants = {
+    openWeatherMap:{
+        BASE_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
+        SECRET_KEY: 'eef1a736509553c562e025e8c69d4e6b'
+    }
+}
+module.exports = constants;
